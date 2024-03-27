29e87cf07d668058de2af7f91208e65d7d222d8e
Aiden Lambert <aiden.lambert@ucalgary.ca>
2024-03-27 00:12:53 -0600

    Added states for username, login, password and email.

--- my-ecommerce-app/src/component/LoginForm.js
+++ my-ecommerce-app/src/component/LoginForm.js
@@ -1,8 +1,16 @@
 import React, {useState} from 'react';
 
 function LoginForm() {
-    const [signup, setSignup] = useState(false);
-    
+    const [signup, setSignup] = useState(false); // variable to toggle between login and signup form
+
+    const [username, setUsername] = useState(''); //  variable to store username
+    const [password, setPassword] = useState(''); // variable to store password
+    const [confirmPassword, setConfirmPassword] = useState(''); // variable to store confirm password
+    const [email, setEmail] = useState(''); //  variable to store email
+
+    const handleAuthentication = (e) => {return} // todo implement this function
+    const handleSignup = (e) => {return} // todo implement this function
+
     return(
         <div>
             <h2>Login</h2>
@@ -10,10 +18,10 @@ function LoginForm() {
                 {!signup &&
                     (<form className="login-form">
                         <label>Username</label>
-                        <input type="text" placeholder="Enter your username"  required/><br/>
+                        <input type="text" placeholder="Enter your username" required onChange={(e) =>setUsername(e.target.value)}/><br/>
                         <label>Password</label>
-                        <input type="password" placeholder="Enter your password"  required/><br/>
-                        <button type="submit">Login</button>
+                        <input type="password" placeholder="Enter your password" required onChange={(e) =>setPassword(e.target.value)}/><br/>
+                        <button type="submit" onClick={handleAuthentication}>Login</button>
                         <button onClick={() => setSignup(!signup)}className="signup">Switch to Signup</button>
                     </form>)}
             <div/>
@@ -21,14 +29,14 @@ function LoginForm() {
                 {signup && 
                     (<form className="login-form">
                     <label>Username</label>
-                    <input type="text" placeholder="Enter your username"  required/><br/>
+                    <input type="text" placeholder="Enter your username" required onChange={(e) =>setUsername(e.target.value)}/><br/>
                     <label>Password</label>
-                    <input type="password" placeholder="Enter your password" required /><br/>
+                    <input type="password" placeholder="Enter your password" required onChange={(e) =>setPassword(e.target.value)} /><br/>
                     <label>Confirm Password</label>
-                    <input type="password" placeholder="Confirm your password"  required/><br/>
+                    <input type="password" placeholder="Confirm your password" required onChange={(e) =>setConfirmPassword(e.target.value)}/><br/>
                     <label>Email</label>
-                    <input type="email" placeholder="Enter your email" required /><br/>
-                    <button type="submit">Signup</button>
+                    <input type="email" placeholder="Enter your email" required onChange={(e) =>setEmail(e.target.value)} /><br/>
+                    <button type="submit" onClick={handleSignup}>Signup</button>
                     <button onClick={() => setSignup(!signup)} className="signup">Switch to Login</button>
 
                 </form>)}
